28aa63e560fcbd5ac225cf5bf328e38c926b63d7
Neal Hardesty <neal@crunchbase.com>
2024-09-17 16:16:05 -0700

    Added local logging

--- main.go
+++ main.go
@@ -14,29 +14,44 @@ var (
 )
 
 func echoHandler(w http.ResponseWriter, r *http.Request) {
+	var s string
+
+	fmt.Println("---- Request ----")
+
 	// Print method and URL
-	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL.Path, r.Proto)
+	s = fmt.Sprintf("%s %s %s\n", r.Method, r.URL.Path, r.Proto)
+	fmt.Fprint(w, s)
+	fmt.Print(s)
 
 	// Print headers
 	for name, values := range r.Header {
 		for _, value := range values {
-			fmt.Fprintf(w, "%s: %s\n", name, value)
+			s = fmt.Sprintf("%s: %s\n", name, value)
+			fmt.Fprint(w, s)
+			fmt.Print(s)
 		}
 	}
 
 	// Print remote ip address
-	fmt.Fprintf(w, "x-goparrot-RemoteAddr: %s\n", r.RemoteAddr)
+	s = fmt.Sprintf("x-goparrot-RemoteAddr: %s\n", r.RemoteAddr)
+	fmt.Fprint(w, s)
+	fmt.Print(s)
 
 	// Print my ip address
-	fmt.Fprintf(w, "x-goparrot-LocalAddr: %s\n", r.Context().Value(http.LocalAddrContextKey))
+	s = fmt.Sprintf("x-goparrot-LocalAddr: %s\n", r.Context().Value(http.LocalAddrContextKey))
+	fmt.Fprint(w, s)
+	fmt.Print(s)
 
 	// Print body
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Unable to read body", http.StatusInternalServerError)
+		fmt.Println("Unable to read body")
 		return
 	}
-	fmt.Fprintf(w, "\n%s", body)
+	s = fmt.Sprintf("\n%s", body)
+	fmt.Fprint(w, s)
+	fmt.Print(s)
 }
 
 func main() {
